b771fe73779d0f35d78dbdf6e26c371845f2cf6d
EmmaAdabra <adabraemma@gmail.com>
2024-10-28 18:40:19 -0400

    Add App class
    This class is the entry point for the app

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import infrastructure.DefaultCSVHandler;
+import infrastructure.ICSVHandler;
+import model.Student;
+import util.CSVHelpers;
+
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
