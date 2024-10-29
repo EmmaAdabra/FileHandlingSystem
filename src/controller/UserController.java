1169c7f7a8400e302b8860790d8ab5ee5890bbb3
EmmaAdabra <adabraemma@gmail.com>
2024-10-29 04:17:22 -0400

    feat: Add some fields to UserController class, and a constructor

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -1,4 +1,23 @@
 package controller;
 
+import model.User;
+import repository.IStudentRepository;
+import repository.IUserRepository;
+import services.UserServices;
+import util.ValidateUserInput;
+
 public class UserController {
+    User currentUSer;
+    ValidateUserInput validate;
+    UserServices userServices;
+
+    public UserController(ValidateUserInput validate, UserServices userServices,
+                          IUserRepository userRepository, IStudentRepository studentRepository) {
+        this.validate = validate;
+        this.userServices = userServices;
+    }
+
+    public void signUp(){
+
+    }
 }
