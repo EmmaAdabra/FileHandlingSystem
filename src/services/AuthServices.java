db26397f233472a95c0a8df4636fc13e89c65a04
EmmaAdabra <adabraemma@gmail.com>
2024-10-29 07:52:48 -0400

    feat: Implement verifyLogin contract method

--- src/services/AuthServices.java
+++ src/services/AuthServices.java
@@ -1,4 +1,24 @@
 package services;
 
-public class AuthServices {
+import model.User;
+import repository.IUserRepository;
+import util.Response;
+
+public class AuthServices implements IAuthServices {
+    IUserRepository userRepository;
+
+    public AuthServices(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public Response verifyLogin(String email, String password) {
+        User user = userRepository.getUser(email);
+
+        if(user == null || !user.getPassword().equals(password)) {
+            return new Response(false, "Invalid user details", null);
+        }
+
+        return new Response(true, "success", user);
+    }
 }
