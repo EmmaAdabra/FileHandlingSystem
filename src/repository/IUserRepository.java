115f8801014a6b6b16aff621e24d66e01b31981c
EmmaAdabra <adabraemma@gmail.com>
2024-10-29 05:20:30 -0400

    feat: add getAllUser contract method

--- src/repository/IUserRepository.java
+++ src/repository/IUserRepository.java
@@ -2,7 +2,10 @@ package repository;
 
 import model.User;
 
+import java.util.List;
+
 public interface IUserRepository {
     User getUser(String email);
     void addUser(User newUser);
+    List<User> getAllUsers();
 }
