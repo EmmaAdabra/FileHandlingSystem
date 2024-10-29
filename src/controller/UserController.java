a3d960a255cc9d7f0bcf8e0fbf9c586545f127e2
EmmaAdabra <adabraemma@gmail.com>
2024-10-29 04:58:59 -0400

    feat: Add signUp method in UserController class
    -This method get details and register them.

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -3,21 +3,41 @@ package controller;
 import model.User;
 import repository.IStudentRepository;
 import repository.IUserRepository;
+import services.IUserServices;
 import services.UserServices;
+import util.IterateInput;
 import util.ValidateUserInput;
 
 public class UserController {
     User currentUSer;
     ValidateUserInput validate;
-    UserServices userServices;
+    IUserServices userServices;
 
-    public UserController(ValidateUserInput validate, UserServices userServices,
-                          IUserRepository userRepository, IStudentRepository studentRepository) {
+    public UserController(ValidateUserInput validate, IUserRepository userRepository,
+                          IStudentRepository studentRepository) {
         this.validate = validate;
-        this.userServices = userServices;
+        this.userServices = new UserServices(studentRepository, userRepository);
     }
 
     public void signUp(){
+        System.out.println();
+        System.out.println("Enter your details");
 
+//        get user details
+        String name = IterateInput.stringInput("Name", validate::validateName);
+        String email = IterateInput.stringInput("Email", validate::validateEmail).toLowerCase();
+        String password = IterateInput.stringInput("Password", validate::validatePassword);
+
+//        create user
+        var newUser = new User(name, email, password);
+
+//       register user
+        boolean register = userServices.addUser(newUser);
+
+        if(register) {
+            System.out.println(newUser.getName() + " have been registered successfully");
+        } else {
+            System.out.println(newUser.getEmail() + " already exist");
+        }
     }
 }
