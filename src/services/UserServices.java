ffccc5bff00e1ddbaf38181e1771bbfd0bceeb31
EmmaAdabra <adabraemma@gmail.com>
2024-10-29 04:14:28 -0400

    feat: studentRepository and userRepository to UserService class - Also add a constructor.

--- src/services/UserServices.java
+++ src/services/UserServices.java
@@ -1,5 +1,15 @@
 package services;
 
+import repository.IStudentRepository;
+import repository.IUserRepository;
+
 public class UserServices {
+    IStudentRepository studentRepository;
+    IUserRepository userRepository;
+
+    public UserServices(IStudentRepository studentRepository, IUserRepository userRepository) {
+        this.studentRepository = studentRepository;
+        this.userRepository = userRepository;
+    }
 }
 
