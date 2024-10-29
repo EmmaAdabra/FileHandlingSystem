8a80e00602fff1003943858948333ae681c9efb0
EmmaAdabra <adabraemma@gmail.com>
2024-10-29 06:22:04 -0400

    feat: Instantiate couples of classes and call few methods for test purposes

--- src/App.java
+++ src/App.java
@@ -1,12 +1,24 @@
+import controller.UserController;
 import infrastructure.DefaultCSVHandler;
 import infrastructure.ICSVHandler;
 import model.Student;
+import repository.IStudentRepository;
+import repository.IUserRepository;
+import repository.StudentRepository;
+import repository.UserRepository;
 import util.CSVHelpers;
+import util.ValidateUserInput;
 
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
+        var validateUserInput = new ValidateUserInput();
+        IUserRepository userRepository = new UserRepository();
+        IStudentRepository studentRepository = new StudentRepository();
+        var userController = new UserController(validateUserInput, userRepository, studentRepository);
 
+        userController.signUp();
+        userController.displayMenu();
     }
 }
