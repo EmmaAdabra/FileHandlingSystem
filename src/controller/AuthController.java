f7e36ff40171d9a427d8ba3ce0c0607bcff3c83c
EmmaAdabra <adabraemma@gmail.com>
2024-10-29 07:55:27 -0400

    feat: add ValidateUserInput, UserController, IAuthServices fields a constructor method and a login method

--- src/controller/AuthController.java
+++ src/controller/AuthController.java
@@ -1,5 +1,40 @@
 package controller;
 
-public class AuthController {
+import model.User;
+import repository.IStudentRepository;
+import repository.IUserRepository;
+import services.AuthServices;
+import services.IAuthServices;
+import util.IterateInput;
+import util.Response;
+import util.ValidateUserInput;
 
+public class AuthController {
+    private final ValidateUserInput validateInput;
+    private final UserController controller;
+    private final IAuthServices authServices;
+
+    public AuthController(ValidateUserInput validateInput, IUserRepository userRepository,
+                         UserController controller ) {
+        this.validateInput = validateInput;
+        this.controller = controller;
+        this.authServices =  new AuthServices(userRepository);
+    }
+
+    public void login(){
+        System.out.println();
+        System.out.println("--------------- Login Details ---------------");
+        String email = IterateInput.stringInput("email", validateInput::validateEmail);
+        String password = IterateInput.stringInput("password", validateInput::validatePassword);
+
+        Response verify = authServices.verifyLogin(email, password);
+        if(verify.status) {
+            User loggedInUser = (User)verify.obj;
+            controller.displayMenu();
+
+        } else {
+            System.out.println();
+            System.out.println(verify.message);
+        }
+    }
 }
