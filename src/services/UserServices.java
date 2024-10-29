0e81647d1a1761a421710ffbfa633ae1d8514822
EmmaAdabra <adabraemma@gmail.com>
2024-10-29 05:02:39 -0400

    feat: Implement addUser contract methods

--- src/services/UserServices.java
+++ src/services/UserServices.java
@@ -1,15 +1,28 @@
 package services;
 
+import model.User;
 import repository.IStudentRepository;
 import repository.IUserRepository;
 
-public class UserServices {
+public class UserServices implements IUserServices {
     IStudentRepository studentRepository;
     IUserRepository userRepository;
 
     public UserServices(IStudentRepository studentRepository, IUserRepository userRepository) {
         this.studentRepository = studentRepository;
         this.userRepository = userRepository;
+    }
+
+    @Override
+    public boolean addUser(User newUser) {
+        var user = userRepository.getUser(newUser.getEmail());
+
+        if(user == newUser) {
+            userRepository.addUser(newUser);
+            return true;
+        }
+
+        return false;
     }
 }
 
