971bd63cb00b1820fe5c5aced955e5dc5f46af5f
EmmaAdabra <adabraemma@gmail.com>
2024-10-28 20:50:38 -0400

    feat: add CustomerScanner class
    - This class contains static methods used to get user input

---
+++ src/util/CustomScanner.java
@@ -0,0 +1,52 @@
+package util;
+
+import java.util.Scanner;
+
+/**
+ * This class is used to read users inputs
+ */
+public class CustomScanner {
+    private static Scanner scanner = new Scanner(System.in);
+    private static ValidateUserInput validate = new ValidateUserInput();
+    public static String readString(String prompt){
+        String value;
+        while(true) {
+            System.out.print(prompt +": ");
+            value = scanner.nextLine().toUpperCase().trim();
+            if(validate.validateEmptyInput(value)){
+                System.out.println(prompt + " can't be empty");
+            }
+            else {
+                break;
+            }
+        }
+        return value;
+    }
+
+    public static int readInt(String prompt){
+        int option;
+
+        while (true) {
+            System.out.print(prompt +": ");
+            String userOption = scanner.nextLine().trim();
+
+            if(validate.validateEmptyInput(userOption)){
+                System.out.println(prompt + " can't be empty");
+                continue;
+            }
+
+            var response = validate.validateIntInput(userOption);
+
+            if(!response.status) {
+                System.out.println("Only accept numbers");
+                continue;
+            }
+
+            option = (Integer) response.obj;
+
+            break;
+
+        }
+        return option;
+    }
+}
