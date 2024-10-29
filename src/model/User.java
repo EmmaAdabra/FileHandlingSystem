0655f7a922c3ed779c5186ca6a4a1206638dcb9c
EmmaAdabra <adabraemma@gmail.com>
2024-10-29 05:19:26 -0400

    feat: override toString method to display user details in a readable format

--- src/model/User.java
+++ src/model/User.java
@@ -36,4 +36,9 @@ public class User {
     public int hashCode() {
         return Objects.hash(getEmail());
     }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + "\nEmail: " + email;
+    }
 }
