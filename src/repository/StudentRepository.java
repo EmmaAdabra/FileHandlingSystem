4d0cc14910bbad1e927e903583b4269e39818f01
EmmaAdabra <adabraemma@gmail.com>
2024-10-30 05:22:07 -0400

    feat: implement loadStudentCSV contacted methods - add csvHandler and students fields

--- src/repository/StudentRepository.java
+++ src/repository/StudentRepository.java
@@ -1,4 +1,31 @@
 package repository;
 
+import infrastructure.ICSVHandler;
+import model.Student;
+import util.Response;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentRepository implements IStudentRepository  {
+    private final ICSVHandler csvHandler;
+
+    private final List<Student> students = new ArrayList<>();
+
+    public StudentRepository(ICSVHandler csvHandler) {
+        this.csvHandler = csvHandler;
+    }
+
+    @Override
+    public Response loadStudentCSV() {
+        Response response = csvHandler.readStudentFromCSV();
+
+        if(response.status) {
+//            Note: modify logic later
+            List<Student> studentRecords = (List<Student>) response.obj;
+            students.addAll(studentRecords);
+            return new Response(true, "success", null);
+        }
+        return response;
+    }
 }
