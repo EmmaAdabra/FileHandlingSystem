6b49a138a6149b5b4f0cdb0c2f5ef0e373193e14
EmmaAdabra <adabraemma@gmail.com>
2024-10-30 05:59:37 -0400

    feat: add getStudents contract method

--- src/repository/IStudentRepository.java
+++ src/repository/IStudentRepository.java
@@ -1,8 +1,12 @@
 package repository;
 
 import infrastructure.ICSVHandler;
+import model.Student;
 import util.Response;
+
+import java.util.List;
 
 public interface IStudentRepository {
     Response loadStudentCSV();
+    List<Student> getStudents();
 }
