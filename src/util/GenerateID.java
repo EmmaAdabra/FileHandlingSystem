18382d8e6005217c056e4915dfb14e0a90f67df9
EmmaAdabra <adabraemma@gmail.com>
2024-10-30 11:36:04 -0400

    fix: Add some check for extreme cases

--- src/util/GenerateID.java
+++ src/util/GenerateID.java
@@ -2,19 +2,24 @@ package util;
 
 public class GenerateID {
     public static String generateID(String name, int studentSize){
-        if(name != null) {
-            String[] names = name.split(" ");
-            String nameInitial = String.valueOf(names[0].charAt(0)).toUpperCase()
-                    + String.valueOf(names[1].charAt(0)).toUpperCase();
-
-            int IDNumber = (studentSize * 10) / 10;
-
-            String numberPartOfID = IDNumber < 10 ? "00"+studentSize : IDNumber < 100 ? "0" + studentSize:
-                    "" + studentSize;
-
-            return nameInitial + numberPartOfID;
+        if(name == null) {
+            return null;
         }
 
-       return null;
+        String[] names = name.split(" ");
+
+        if(names.length < 2) {
+            return null;
+        }
+
+        String nameInitial = String.valueOf(names[0].charAt(0)).toUpperCase()
+                + String.valueOf(names[1].charAt(0)).toUpperCase();
+
+        int IDNumber = (studentSize * 10) / 10;
+
+        String numberPartOfID = IDNumber < 10 ? "00"+studentSize : IDNumber < 100 ? "0" + studentSize:
+                "" + studentSize;
+
+        return nameInitial + numberPartOfID;
     }
 }
