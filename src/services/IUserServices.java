32520992a99993c63c589d725bcb897b1c851147
EmmaAdabra <adabraemma@gmail.com>
2024-10-30 05:22:50 -0400

    feat: add setCSVHandler contract method

--- src/services/IUserServices.java
+++ src/services/IUserServices.java
@@ -1,10 +1,13 @@
 package services;
 
 import model.User;
+import util.Response;
 
 import java.util.List;
 
 public interface IUserServices {
     boolean addUser(User user);
     List<User> getUsers();
+    Response setCSVHandler(String handler);
 }
+
