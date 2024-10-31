9f012dfcebebcf3a9e946b7c22c4636741b898d7
EmmaAdabra <adabraemma@gmail.com>
2024-10-30 20:48:12 -0400

    chore: Modify generateID method and add few new methods to improve the logic

--- src/util/GenerateID.java
+++ src/util/GenerateID.java
@@ -1,7 +1,13 @@
 package util;
 
+import model.Student;
+
+import java.util.List;
+
 public class GenerateID {
-    public static String generateID(String name, int studentSize){
+    public static String generateID(String name, List<Student> students){
+        int studentSize = students.size();
+
         if(name == null) {
             return null;
         }
@@ -15,11 +21,26 @@ public class GenerateID {
         String nameInitial = String.valueOf(names[0].charAt(0)).toUpperCase()
                 + String.valueOf(names[1].charAt(0)).toUpperCase();
 
+
+       String studentID = computeID(nameInitial, studentSize);
+
+        while (isIDExists(studentID, students)) {
+            studentID = computeID(nameInitial, studentSize+1);
+        }
+
+        return studentID;
+    }
+
+    private static String computeID(String nameInitial, int studentSize){
         int IDNumber = (studentSize * 10) / 10;
 
         String numberPartOfID = IDNumber < 10 ? "00"+studentSize : IDNumber < 100 ? "0" + studentSize:
                 "" + studentSize;
 
         return nameInitial + numberPartOfID;
+    }
+
+    public static boolean isIDExists(String id, List<Student> students) {
+        return students.stream().anyMatch(student -> student.getId().equals(id));
     }
 }
