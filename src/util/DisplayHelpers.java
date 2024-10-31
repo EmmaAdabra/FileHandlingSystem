54017409940b87169dd94941617f9607c619a872
EmmaAdabra <adabraemma@gmail.com>
2024-10-30 23:46:09 -0400

    refactor: uncomment on line 23

--- src/util/DisplayHelpers.java
+++ src/util/DisplayHelpers.java
@@ -20,7 +20,7 @@ public class DisplayHelpers {
     }
 
     public static void displaySubMenu(String title, String[] options){
-//        System.out.println();
+        System.out.println();
         System.out.println(title);
         for(int i = 0; i < options.length; i++) {
             System.out.println(i+1 + ". " + options[i]);
