345e4645aeac61fe5e7a7aa6e86a4964089d817a
EmmaAdabra <adabraemma@gmail.com>
2024-10-30 23:47:08 -0400

    feat: Add getStudentByID and updateStudentRecord contract methods

--- src/repository/IStudentRepository.java
+++ src/repository/IStudentRepository.java
@@ -4,11 +4,14 @@ import model.Student;
 import util.Response;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IStudentRepository {
     Response loadStudentCSV();
     List<Student> getStudents();
     void addStudent(Student student);
     boolean isStudent(Student student);
+    Optional<Student> getStudentByID(String id);
+    Response updateStudentRecord();
 }
 
